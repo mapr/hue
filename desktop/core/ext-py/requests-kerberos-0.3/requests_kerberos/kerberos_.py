f553e3a951c26cfab9fe4fba32224a1cb09ac6ce
Roman Slysh <rslysh@maprtech.com>
2015-03-16 15:17:41 +0200

    [FIX]MAPR-13476 fix fake error in logging

--- desktop/core/ext-py/requests-kerberos-0.3/requests_kerberos/kerberos_.py
+++ desktop/core/ext-py/requests-kerberos-0.3/requests_kerberos/kerberos_.py
@@ -192,8 +192,9 @@ class HTTPKerberosAuth(AuthBase):
                 return response
 
             elif is_http_error or self.mutual_authentication == OPTIONAL:
-                log.error("handle_other(): Mutual authentication unavailable "
-                          "on {0} response".format(response.status_code))
+                if response.status_code != 200:
+                    log.error("handle_other(): Mutual authentication unavailable "
+                              "on {0} response".format(response.status_code))
 
                 if self.mutual_authentication == REQUIRED:
                     return SanitizedResponse(response)
