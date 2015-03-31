c88094097001ac76baf38f8453d8706f4c9d27c5
ababychCV <ababych@maprtech.com>
2015-03-31 15:01:04 +0300

    Impala should not execute 'invalidate matadata' requests with removed tables
    Signed-off-by: Igor Varga <ivarga@cybervisiontech.com>
    Signed-off-by: IVarha <ivarga@maprtech.com>

--- apps/impala/src/impala/views.py
+++ apps/impala/src/impala/views.py
@@ -43,9 +43,8 @@ def refresh_tables(request):
     try:
       database = json.loads(request.POST['database'])
       added = json.loads(request.POST['added'])
-      removed = json.loads(request.POST['removed'])
 
-      db.invalidate_tables(database, added + removed)
+      db.invalidate_tables(database, added)
     except Exception, e:
       response['message'] = str(e)
 
