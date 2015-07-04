340cff5370684ec907f228fa1aa17500d829ca42
IVarha <ivarga@maprtech.com>
2015-07-04 15:40:52 +0300

    MAPR-18650 [hbase] Mapr-db table browser doesn't update with new table
    Signed-off-by: IVarha <ivarga@maprtech.com>
    (cherry picked from commit f8a3dd9)
    (cherry picked from commit 9a91910)
    
    (cherry picked from commit 4ed14fa)

--- apps/hbase/src/hbase/static/hbase/js/maprtables.js
+++ apps/hbase/src/hbase/static/hbase/js/maprtables.js
@@ -8,7 +8,25 @@ function getCurrentCluster() {
 
 }
 $(function () {
-    $('#jstree_mapr').jstree({
+    var tree_id = "#jstree_mapr";
+    var refreshable = "needrefresh";
+    $(tree_id).bind("open_node.jstree close_node.jstree", function (e,data) {
+        var tree = jQuery.jstree.reference(tree_id);
+        var currentNode = data.node.id;
+        if(e.type === "close_node") {
+            $(tree_id).jstree(true).get_node(currentNode).class = refreshable;
+        }
+        if(e.type === "open_node") {
+            try{
+                if (refreshable === $(tree_id).jstree(true).get_node(currentNode).class ) {
+                    $(tree_id).jstree(true).get_node(currentNode).class = undefined;
+                    $(tree_id).jstree(true).refresh_node(currentNode);
+                }
+            }
+            catch(e){
+            }
+        }
+    }).jstree({
         'core' : {
             'data' : {
                 'url' : 'getlist/',
