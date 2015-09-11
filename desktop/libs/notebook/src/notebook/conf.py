f3a3f4986a98acd8904c96de569732dfede3c070
Romain Rigaux <romain@cloudera.com>
2015-09-11 16:13:33 -0400

    [notebook] Make snippets configrable from hue.ini

--- desktop/libs/notebook/src/notebook/conf.py
+++ desktop/libs/notebook/src/notebook/conf.py
@@ -19,7 +19,7 @@ import json
 
 from django.utils.translation import ugettext_lazy as _t
 
-from desktop.lib.conf import Config
+from desktop.lib.conf import Config, UnspecifiedConfigSection, ConfigSection
 
 
 def coerce_json(j):
@@ -40,4 +40,32 @@ LANGUAGES = Config(
       {"name": "Python", "type": "py"},
       {"name": "Text", "type": "text"}
   ]"""
+)
+
+INTERPRETERS = UnspecifiedConfigSection(
+  "interpreters",
+  help="One entry for each type of snippet",
+  each=ConfigSection(
+    help="Information about a single Zookeeper cluster",
+    members=dict(
+      HOST_PORTS=Config(
+          "name",
+          help="Zookeeper ensemble. Comma separated list of Host/Port, e.g. localhost:2181,localhost:2182,localhost:2183",
+          default="localhost:2181",
+          type=str,
+      ),
+      REST_URL=Config(
+          "type",
+          help="The URL of the REST contrib service.",
+          default="http://localhost:9998",
+          type=str,
+      ),
+      PRINCIPAL_NAME=Config(
+          "interface",
+          help="Name of Kerberos principal when using security",
+          default="zookeeper",
+          type=str,
+      ),
+    )
+  )
 )
