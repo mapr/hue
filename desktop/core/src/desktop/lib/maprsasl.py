6680f040052dfb9b2e9bae12afc63f9b1653b3db
Ivan Dzikovskyi <idzikovskyi@mapr.com>
2019-03-11 16:31:36 +0200

    Codestyle fixes in maprsasl.py

--- desktop/core/src/desktop/lib/maprsasl.py
+++ desktop/core/src/desktop/lib/maprsasl.py
@@ -1,34 +1,47 @@
+import base64
 import logging
 import os
+from requests.auth import AuthBase
+import security_pb2
+import struct
 
 LOG = logging.getLogger(__name__)
 
 try:
     import maprsecurity
-except ImportError as e:
-    msg = ("Could not import maprsecurity module.\n" +
-           "Any attempt to use MaprSasl will cause an exception. Please, ensure that\n" +
-           "1) path to maprsecurity.so in PYTHONPATH (typically $MAPR_HOME/libexp);\n" +
-           "2) path to libMapRClient.so and libjvm.so in LD_LIBRARY_PATH " +
-           "(typically $MAPR_HOME/lib and $JRE_HOME/lib/amd64/server respectively).\n" +
-           "%s"
-           ) % (e, )
+except ImportError as err:
+    # This problem typically happens only on build machines. Handle it to prevent build failure.
+    msg = ("Could not import maprsecurity module.\n"
+           "Any attempt to use MaprSasl will cause an exception. Please, ensure that\n"
+           "1) path to maprsecurity.so in PYTHONPATH (typically $MAPR_HOME/libexp);\n"
+           "2) path to libMapRClient.so and libjvm.so in LD_LIBRARY_PATH "
+           "(typically $MAPR_HOME/lib and $JRE_HOME/lib/amd64/server respectively).\n"
+           "{}"
+           ).format(err)
     LOG.exception(msg)
 
-from requests.auth import AuthBase
-import security_pb2
-import base64
-
 MAPR_HOME = os.environ.get('MAPR_HOME', '/opt/mapr')
-CONF_FILE = os.path.join(MAPR_HOME, 'conf', 'mapr-clusters.conf')
+MAPR_CLUSTERS_CONF_FILE = os.path.join(MAPR_HOME, 'conf', 'mapr-clusters.conf')
 
-def get_cluster_name():
-    return open(CONF_FILE, 'r').read().split()[0]
+try:
+    with open(MAPR_CLUSTERS_CONF_FILE, 'r') as mapr_clusters_conf:
+        MAPR_CLUSTER_NAME = mapr_clusters_conf.read().split()[0]
+except IOError as err:
+    # This problem typically happens only on build machines. Handle it to prevent build failure.
+    msg = ("Could not open {}.\n"
+           "Using default cluster name 'my.cluster.com'.\n"
+           "{}"
+           ).format(MAPR_CLUSTERS_CONF_FILE, err)
+    LOG.exception(msg)
+    MAPR_CLUSTER_NAME = 'my.cluster.com'
+
 
 class MaprSasl(object):
 
     def __init__(self):
-        self.sessionKey = ''
+        self.session_key = ''
+        self.random_number = None
+        self.ticket_and_key = None
 
     def init(self):
         pass
@@ -37,62 +50,58 @@ class MaprSasl(object):
         pass
 
     def get_init_response(self):
-        serverKeyBytes = maprsecurity.GetTicketAndKeyForClusterInternal(get_cluster_name(), 1)
-        tk = security_pb2.TicketAndKey()
-        tk.ParseFromString(serverKeyBytes)
+        server_key_bytes = maprsecurity.GetTicketAndKeyForClusterInternal(MAPR_CLUSTER_NAME, 1)
 
-        self.randomNumber = maprsecurity.GenerateRandomNumber()
-        import struct
-        encr = maprsecurity.Encrypt(tk.userKey.key, struct.pack('l', self.randomNumber)[::-1])
-        if (self.randomNumber < 0): self.randomNumber += (1 << 64)
-        auth = security_pb2.AuthenticationReqFull()
-        auth.encryptedRandomSecret = encr
-        auth.encryptedTicket = tk.encryptedTicket
+        self.ticket_and_key = security_pb2.TicketAndKey()
+        self.ticket_and_key.ParseFromString(server_key_bytes)
 
-        authRequestBytes = base64.b64encode(auth.SerializeToString())
-        self.tk = tk
-        return authRequestBytes
+        self.random_number = maprsecurity.GenerateRandomNumber()
+        encrypted_random_secret = maprsecurity.Encrypt(self.ticket_and_key.userKey.key,
+                                                       struct.pack('l', self.random_number)[::-1])
+        if self.random_number < 0:
+            self.random_number += (1 << 64)
+
+        auth_req = security_pb2.AuthenticationReqFull()
+        auth_req.encryptedRandomSecret = encrypted_random_secret
+        auth_req.encryptedTicket = self.ticket_and_key.encryptedTicket
+
+        auth_request_bytes = base64.b64encode(auth_req.SerializeToString())
+        return auth_request_bytes
 
     def start(self, mechanism):
-        ret = True
-        chosen_mech = mechanism
-        initial_response = self.get_init_response()
-        return ret, chosen_mech, initial_response
+        return True, mechanism, self.get_init_response()
 
     def getError(self):
         return -1
 
     def step(self, payload):
-        token = payload
-        challenge = base64.b64decode(token)
-        decodedResponse = maprsecurity.Decrypt(self.tk.userKey.key, challenge)
-        authResponse = security_pb2.AuthenticationResp()
-        authResponse.ParseFromString(decodedResponse)
-        # auth = 1, auth-int = 2, auth-conf = 4
-        qopInt = authResponse.encodingType
-        self.sessionKey = '' if qopInt == 1 else authResponse.sessionKey.key
-        result = authResponse.challengeResponse == self.randomNumber
+        challenge = base64.b64decode(payload)
+        decoded_response = maprsecurity.Decrypt(self.ticket_and_key.userKey.key, challenge)
+        auth_response = security_pb2.AuthenticationResp()
+        auth_response.ParseFromString(decoded_response)
+        # Quality-of-Protection: auth = 1, auth-int = 2, auth-conf = 4
+        self.session_key = '' if auth_response.encodingType == 1 else auth_response.sessionKey.key
+        result = auth_response.challengeResponse == self.random_number
         return result, ''
 
     def encode(self, data):
-      import struct
-      if self.sessionKey == '':
-        return True, data
+        if self.session_key == '':
+            return True, data
 
-      encodedData = maprsecurity.Encrypt(self.sessionKey, data)
-      return True, struct.pack(">I", len(encodedData)) + encodedData
+        encoded_data = maprsecurity.Encrypt(self.session_key, data)
+        return True, struct.pack(">I", len(encoded_data)) + encoded_data
 
     def decode(self, data):
-      if self.sessionKey == '':
-        return True, data
+        if self.session_key == '':
+            return True, data
 
-      # We need cut header (4 bytes)
-      decodedData = maprsecurity.Decrypt(self.sessionKey, data[4:])
-      return True, decodedData
+        # We need cut header (4 bytes)
+        decoded_data = maprsecurity.Decrypt(self.session_key, data[4:])
+        return True, decoded_data
+
 
 class HttpMaprAuth(AuthBase):
     def __call__(self, request):
         mapr = MaprSasl()
         request.headers['Authorization'] = 'MAPR-Negotiate ' + mapr.get_init_response()
         return request
-
