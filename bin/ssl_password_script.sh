29951d569c25a0cac2336a93cce22df83a5c5bac
Mykhailo Kysliuk <mkysliuk@mapr.com>
2020-07-17 21:36:38 +0300

    MHUE-400: Hue-4.6 not starting on Centos-8.1 (mapr-sasl).

---
+++ bin/ssl_password_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MAPR_HOME=${MAPR_HOME:-"/opt/mapr"}
+SSL_SERVER_CONFIG_FILE="${MAPR_HOME}/conf/ssl-server.xml"
+SSL_KEYSTORE_PASSWORD_PROP="ssl.server.keystore.password"
+
+get_property_value() {
+  local property_name=$1
+  sed -n '/'${property_name}'/{:a;N;/<\/value>/!ba {s|.*<value>\(.*\)</value>|\1|p}}' "$SSL_SERVER_CONFIG_FILE"
+}
+
+get_property_value ssl.server.keystore.password
