710dd038ecfc0028592bc25981b616cf21ba287b
Ivan Dzikovskyi <ivan.dzikovskyi@hpe.com>
2024-08-15 16:23:38 +0300

    Update Python version shipped in with Hue package

--- devops/scripts/_initialize_package_variables.sh
+++ devops/scripts/_initialize_package_variables.sh
@@ -12,7 +12,7 @@ DIST_DIR=${DIST_DIR:-"devops/dist"}
 # rpmbuild does not work properly when relatve path specified here
 BUILD_ROOT=${BUILD_ROOT:-"$(pwd)/devops/buildroot"}
 
-PYTHON_VERSION_LONG=${PYTHON_VER_LONG:-"3.8.18"}
+PYTHON_VERSION_LONG=${PYTHON_VER_LONG:-"3.8.19"}
 PYTHON_VERSION=$(echo "$PYTHON_VERSION_LONG" | cut -d '.' -f 1-2)
 PYTHON_VER="python${PYTHON_VERSION}"
 PYTHON_SRC=${PYTHON_SRC:-"https://www.python.org/ftp/python/${PYTHON_VERSION_LONG}/Python-${PYTHON_VERSION_LONG}.tar.xz"}
