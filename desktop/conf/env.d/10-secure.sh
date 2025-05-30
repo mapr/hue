76530d7af3faa5c5869f10fbc7780c85c011c61b
Ivan Dzikovskyi <ivan.dzikovskyi@hpe.com>
2025-05-30 19:51:37 +0300

    [porting] Add inital dist files

--- desktop/conf/env.d/10-secure.sh
+++ desktop/conf/env.d/10-secure.sh
@@ -14,3 +14,38 @@ if [ "$hue_secure" = "true" ]; then
   export ssl_certificate=${ssl_certificate:-"${MAPR_HOME}/conf/ssl_keystore.pem"}
   export ssl_private_key=${ssl_private_key:-"${MAPR_HOME}/conf/ssl_keystore.pem"}
 fi
+
+#
+# Keycloak integation
+#
+queryjson() {
+  "${HUE_HOME}/build/env/bin/python" -c "import json,sys; print(json.load(sys.stdin)${1})"
+}
+
+SSO_CONF=$(maprcli cluster getssoconf -json 2>/dev/null)
+rc="$?"
+
+if [ "$rc" != 0 ]; then
+  export HUE_AUTH_BACKEND="desktop.auth.backend.PamBackend"
+else
+  export HUE_AUTH_BACKEND="desktop.auth.backend.PamBackend,desktop.auth.backend.OIDCBackend"
+
+  clientid=$(echo "$SSO_CONF" | queryjson '["data"][0]["clientid"]')
+  clientsecret=$(echo "$SSO_CONF" | queryjson '["data"][0]["clientsecret"]')
+  issuerendpoint=$(echo "$SSO_CONF" | queryjson '["data"][0]["issuerendpoint"]')
+
+  hue_url="http://$(hostname -f):8888"
+  if [ "$hue_secure" = "true" ]; then
+    hue_url="https://$(hostname -f):8888"
+  fi
+
+  export HUE_OIDC_RP_CLIENT_ID="$clientid"
+  export HUE_OIDC_RP_CLIENT_SECRET="$clientsecret"
+  export HUE_OIDC_OP_AUTHORIZATION_ENDPOINT="${issuerendpoint}/protocol/openid-connect/auth"
+  export HUE_OIDC_OP_TOKEN_ENDPOINT="${issuerendpoint}/protocol/openid-connect/token"
+  export HUE_OIDC_OP_USER_ENDPOINT="${issuerendpoint}/protocol/openid-connect/userinfo"
+  export HUE_OIDC_OP_JWKS_ENDPOINT="${issuerendpoint}/protocol/openid-connect/certs"
+  export HUE_LOGIN_REDIRECT_URL="${hue_url}/oidc/callback/"
+  export HUE_LOGOUT_REDIRECT_URL="${issuerendpoint}/protocol/openid-connect/logout"
+  export HUE_LOGIN_REDIRECT_URL_FAILURE="${hue_url}/hue/oidc_failed/"
+fi
